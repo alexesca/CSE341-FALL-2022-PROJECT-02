7a65e1bdf5eaa606e44c80f591bb4e277ab254d4
alexesca <alexescamore@gmail.com>
2022-11-16 22:17:18 -0700

    Added routes to index router

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+router.use("/users", require("./users.js"));
+router.use("/technologies", require("./technologies.js"));
+router.use("/scripts", require("./scripts.js"));
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Scripts.io' });
 });
