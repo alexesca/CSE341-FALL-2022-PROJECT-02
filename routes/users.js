10c2f2784015b0dd86f0341d4393f2730ed23214
alexesca <alexescamore@gmail.com>
2022-11-19 23:34:48 -0700

    Added validate user middleware

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,14 @@
-const controller = require("../controllers/users.js");
 const express = require("express");
 const router = express.Router();
+
+const controller = require("../controllers/users.js");
+const validateUserMiddleware = require('./../middlewares/validate-user.middleware.js');
 
 router
     .get("/", controller.index)
     .get("/:_id", controller.id)
-    .post("/", controller.create)
-    .put("/:_id", controller.update)
+    .post("/", validateUserMiddleware.validate, controller.create)
+    .put("/:_id", validateUserMiddleware.validate, controller.update)
     .delete("/:_id", controller.delete);
 
 module.exports = router;
