9ce8d15d39f7ea716d3f4659fe2b714620cc95a3
alexesca <alexescamore@gmail.com>
2022-11-19 23:39:16 -0700

    Added validate technology middleware

---
+++ middlewares/validate-technology.middleware.js
@@ -0,0 +1,11 @@
+const {get} = require('lodash');
+
+exports.validate = (req, res, next) => {
+    const body = get(req, 'body');
+    const name = get(body, 'name');
+    const description = get(body, 'description');
+    const logo = get(body, 'logo');
+
+    if(!name || !description || !logo) throw new Error("All fields are required.");
+
+}
