c44056776ac6f6a7edffbc609a8cc46d4ba8d0b6
alexesca <alexescamore@gmail.com>
2022-11-26 22:02:31 -0700

    Added middleware to login

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const ensureLogIn = require('connect-ensure-login').ensureLoggedIn;
 
 const controller = require("../controllers/users.js");
 const validateUserMiddleware = require('./../middlewares/validate-user.middleware.js');
 const validateIdMiddleware = require('./../middlewares/validate-id.middleware.js');
 
+const ensureLoggedIn = ensureLogIn();
+
 router
-    .get("/", controller.index)
-    .get("/:_id", validateIdMiddleware.validate, controller.id)
-    .post("/", validateUserMiddleware.validate, controller.create)
-    .put("/:_id", validateIdMiddleware.validate,  validateUserMiddleware.validate, controller.update)
-    .delete("/:_id", validateIdMiddleware.validate, controller.delete);
+    .get("/",ensureLoggedIn, controller.index)
+    .get("/:_id",ensureLoggedIn, validateIdMiddleware.validate, controller.id)
+    .post("/",ensureLoggedIn, validateUserMiddleware.validate, controller.create)
+    .put("/:_id",ensureLoggedIn, validateIdMiddleware.validate,  validateUserMiddleware.validate, controller.update)
+    .delete("/:_id",ensureLoggedIn, validateIdMiddleware.validate, controller.delete);
 
 module.exports = router;
