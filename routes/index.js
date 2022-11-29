388308fec4c5a9727664a43ce726aed516f73a51
alexesca <alexescamore@gmail.com>
2022-11-28 23:01:05 -0700

    Moved middleware

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const ScriptsMiddleware = require('./../middlewares/fetch-scripts.middleware.js');
+const authenticatedUserMiddleware = require('./../middlewares/authenticated-user.middleware.js');
+
 
 /* GET home page. */
 router.use("/login", require("./login.js"));
 router.use("/users", require("./users.js"));
 router.use("/technologies", require("./technologies.js"));
-router.use("/scripts", require("./scripts.js"));
+router.use("/scripts", authenticatedUserMiddleware.user, require("./scripts.js"));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
--- routes/scripts.js
+++ routes/scripts.js
@@ -3,15 +3,14 @@ const router = express.Router();
 
 const controller = require("../controllers/scripts.js");
 const validateIdMiddleware = require('./../middlewares/validate-id.middleware.js');
-const authenticatedUserMiddleware = require('./../middlewares/authenticated-user.middleware.js');
 const ensureAuthenticated = require('./../middlewares/ensure-authenticated.middleware.js');
 const bodyUserId = require('./../middlewares/body-user-id.middleware.js');
 
 router
     .get("/:_id",ensureAuthenticated, validateIdMiddleware.validate,  controller.id)
     .get("/", ensureAuthenticated, controller.index)
-    .post("/", ensureAuthenticated, authenticatedUserMiddleware.user, bodyUserId.user, controller.create)
+    .post("/", ensureAuthenticated, bodyUserId.user, controller.create)
     .put("/:_id",ensureAuthenticated, validateIdMiddleware.validate,  controller.update)
-    .post("/:_id",ensureAuthenticated, validateIdMiddleware.validate,  controller.delete);
+    .delete("/:_id",ensureAuthenticated, validateIdMiddleware.validate,  controller.delete);
 
 module.exports = router;
